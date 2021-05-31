789677b3785a3609ad363e10c52739fbec56c2ba
Naoki Yokoyama <naokity@msn.com>
2021-05-31 12:00:29 -0300

    dash3

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -3,6 +3,6 @@ import pandas as pd
 class Dataset:
     
     def __init__(self):
-       self.df = pd.read_csv('dataset\dados_mercado_livre.csv').dropna()
+       self.df = pd.read_csv('https://raw.githubusercontent.com/naokityokoyama/dash/master/dataset/dados_mercado_livre.csv').dropna()
        self.numero = (13,4,52,23,67,82)
        
